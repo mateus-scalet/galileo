974a664196473353b0138c8404f53e864aa5bbdb
mateus-scalet <mateus.scalet@gmail.com>
2025-12-20 17:33:40 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
-  }
-});
+  base: '/galileo/', // Adicione esta linha exatamente assim
+})
